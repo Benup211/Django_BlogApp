492b55a0ea426c2ca21649bf934def1430d9d4a6
benup211 <benup211@gmail.com>
2023-10-05 22:05:20 +0545

    Add Generic Relationship

--- blango/blog/admin.py
+++ blango/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from blog.models import Tag,Post
+from blog.models import Tag,Post,Comment
 # Register your models here.
 admin.site.register(Tag)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}#slug field update when title field is changed
     list_display = ('slug', 'published_at','title')#display inside value in Post outside
 admin.site.register(Post,PostAdmin)
+admin.site.register(Comment)

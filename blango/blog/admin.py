d16ce581b98518c6c857c9629c2db4821053d8bc
benup211 <benup211@gmail.com>
2023-10-05 21:16:50 +0545

    Finished Setting up Model,Admin

---
+++ blango/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from blog.models import Tag,Post
+# Register your models here.
+admin.site.register(Tag)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}#slug field update when title field is changed
+    list_display = ('slug', 'published_at','title')#display inside value in Post outside
+admin.site.register(Post,PostAdmin)
